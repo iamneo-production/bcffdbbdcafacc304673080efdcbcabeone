7b3626f972ec9d45dcde6693785db810fecc8a86
Iamneo <techadmin@iamneo.ai>
2023-09-03 10:00:53 +0000

    Backup Commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-const cells = document.querySelectorAll()
+const cells = document.querySelectorAll('[data-cell]');
+const resultText = document.querySelector('.result-text');
+const resetButton = document.querySelector('.reset-button');
+let currentPlayer = 'X';
+let gameBoard = ['','','','','','','','',''];
+let gameActive = true;
+
+function handleCellClick(e) {
+    const cell = e.target;
+    const cellIndex = parseInt(cell.getAttribute('data-index'));
+    if(gameBoard[cellIndex] !== '' || !gameActive){
+        return;
+    }
+
+    gameBoard[cellIndex] = currentPlayer;
+    cell.textContent = currentPlayer
+}
