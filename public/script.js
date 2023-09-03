fbcb908601c7a22d1c4fac9b63aefff6a1c51aa0
Iamneo <techadmin@iamneo.ai>
2023-09-03 10:15:55 +0000

    Backup Commit

--- public/script.js
+++ public/script.js
@@ -33,9 +33,26 @@ function checkWin(){
     const winPatterns = [
         [0, 1, 2],[3, 4, 5],[6, 7, 8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ];
     
-    for(let pattern of winPatterns)
-    const [a,b,c] = pattern;
-    if(gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]){
-        return true;
+    for(let pattern of winPatterns){
+        const [a,b,c] = pattern;
+        if(gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]){
+            return true;
+        }
     } 
+    return false;
 }
+
+function isBoardFull(){
+    return !gameBoard.includes('');
+}
+
+function resetGame(){
+    gameBoard = ['','','','','','','','',''];
+    currentPlayer = 'X';
+    gameActive = true;
+    cells.forEach(cell => cell.textContent = '');
+    resultText.textContent = 'Player ${currentPlayer}\'s turn';
+    resetButton.disabled = true;
+}
+
+cells.forEach
